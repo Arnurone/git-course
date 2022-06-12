26eacb9b197a38e95947b524ae222aaf2987db72
arnur <arnuronee@gmail.com>
2022-06-12 19:45:38 +0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// let info = {
+// name:"Arn",
+// age:30,
+// address: {
+// 	city:"Temir",
+// 	street:"Mettal",
+// },
+// show(){
+// 	console.log(`${this.name}, ${this.age}, ${this.address.city}`)
+// }
+// }
+// info.show();
+
+let num=1e6;
+console.log(num);
